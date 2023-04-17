0e1b1df25e1934e0373968f152b15f3dc014e92f
Robin Xia <rx2141@nyu.edu>
2023-04-17 15:09:13 -0400

    add write to file

--- forecasting.c
+++ forecasting.c
@@ -22,6 +22,45 @@ int read_csv(const char *filename, double *data) {
     return i;
 }
 
+
+
+void write_helper(char file_name[], const int* alpha_betas, const int* pointers, int len1, int len2, int len3) {
+    // write an array to a file, len1: length of the Smoothed value for month,
+    // len2: total length of each double array, len3: total length of the data array
+    FILE *fptr;
+    fptr = fopen(file_name,"w");
+    if(fptr == NULL)
+    {
+        printf("Error!");
+        exit(1);
+    }
+    // pointers is an array of double pointers, alpha_betas stores pointers which points to double arrays where alpha and beta are stored. iterate the data
+    for (int i = 0; i < len3; ++i) {
+        double* data = pointers[i];    // get data
+        double* info = alpha_betas[i]; // get corresponding alpha and beta
+        double alpha = info[0];
+        double beta = info[1];
+        fprintf(fptr,"Alpha : %f Beta : %f\n",alpha, beta);
+        for (int j = 0; j < len2; ++j) {
+            int month = j + 1;
+            double line = data[j];
+            if (j >= len1) {
+                // Output the forecasted values
+                fprintf(fptr,"\tPredicted sales for month %d: %.2lf\n",month, line);
+            }
+            else {
+                // Output the smoothed values for the first 11 months
+                fprintf(fptr,"\tSmoothed value for month %d: %.2lf\n",month , line);
+            }
+
+        }
+    }
+
+
+    fclose(fptr);
+}
+
+
 void double_exponential_smoothing(const double *data, int n, double alpha, double beta, double *result) {
     double lt[n];
     double bt[n];
@@ -65,9 +104,11 @@ int main(int argc, char *argv[]) {
     double alpha = 0.01;
     double beta = 0.01;
     double t_start = 0.0, t_taken;
-    double result[11 + 3];
-    int range = 99;
 
+    int range = 100;
+    int* pointers = (int*) malloc(sizeof(int) * 99 * 99);
+    int* alpha_betas = (int*) malloc(sizeof(int) * 99 * 99);
+    int idx = 0;// used to fill pointers and alpha_betas
     t_start = omp_get_wtime();
 
     if (threads_count == 0) {
@@ -75,7 +116,11 @@ int main(int argc, char *argv[]) {
             for (int f_b = 1; f_b < 100; ++f_b) {
                 alpha *= f_a;
                 beta *= f_b;
+                double* result = (double*) malloc(sizeof(double) * (11 + 3));
+                double* alpha_beta = (double*) malloc(sizeof(double) * 2);
                 double_exponential_smoothing(data, data_length, alpha, beta, result);
+                pointers[idx] = result;
+                alpha_betas[idx++] = alpha_beta;
             }
         }
     }
@@ -88,7 +133,7 @@ int main(int argc, char *argv[]) {
             int start = tid * step;
             int end = (tid == threads_count - 1) ? range - 1 : (tid + 1) * step;
             for (int f_a = start; f_a <= end; ++f_a) {
-                for (int f_b = start; f_b <= end; ++f_b) {
+                for (int f_b = 1; f_b < range; ++f_b) {
                     alpha *= f_a;
                     beta *= f_b;
                     double_exponential_smoothing(data, data_length, alpha, beta, result);
@@ -109,6 +154,6 @@ int main(int argc, char *argv[]) {
     for (int i = 0; i < 3; i++) {
         printf("Predicted sales for month %d: %.2lf\n", data_length + 1 + i, result[data_length + i]);
     }
-
+    write_helper("output", alpha_betas, pointers, 11, 11 + 3, 99 * 99);
     return 0;
 }
